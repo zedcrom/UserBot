58f93d9dad9df23c11b2cbc1fd23dc5ecd89b462
Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>
2019-01-25 15:39:08 +0530

    Implement .disapprove Hehehe
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -92,3 +92,28 @@ async def approvepm(e):
                 + ")"
                 + " was approved to PM you.",
             )
+
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.disapprove$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.disapprove$"))
+async def disapprovepm(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        try:
+            from userbot.modules.sql_helper.pm_permit_sql import dissprove
+        except:
+            await e.edit("`Running on Non-SQL mode!`")
+            return
+        dissprove(e.chat_id)
+        await e.edit("`Disapproved to PM!`")
+        if LOGGER:
+            aname = await bot.get_entity(e.chat_id)
+            name0 = str(aname.first_name)
+            await bot.send_message(
+                LOGGER_GROUP,
+                "["
+                + name0
+                + "](tg://user?id="
+                + str(e.chat_id)
+                + ")"
+                + " was Disapproved to PM you.",
+            )
