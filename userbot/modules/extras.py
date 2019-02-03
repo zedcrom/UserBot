15cc713132e8ba9d17a1fccddef452c0cdf313f5
Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>
2019-02-03 18:28:13 +0000

    Added Cowsay (+_+)
    lmao im just doing shit
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -105,3 +105,13 @@ async def toilett(e):
         p+=subprocess.run(l, stdout=subprocess.PIPE).stdout.decode()
         p+='```'
         await e.edit(p)
+
+@bot.on(events.NewMessage(pattern='^\.cs (.+)'))
+@bot.on(events.MessageEdited(pattern='^\.cs (.+)'))
+async def cowsay(e):
+        l=['cowsay']
+        l+=e.pattern_match.group(1).split(' ')
+        p='```'
+        p+=subprocess.run(l, stdout=subprocess.PIPE).stdout.decode()
+        p+='```'
+        await e.edit(p)  
