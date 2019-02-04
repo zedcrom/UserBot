75be6fd2238c871f9afe4ef5a3fbea7f5a4d2fc8
zakaryan2004 <zakaryan.2004@outlook.com>
2019-02-04 16:58:01 +0000

    Lemme Google That For you
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -1,9 +1,10 @@
 import asyncio, subprocess
-from userbot import bot
+from userbot import bot, LOGGER, LOGGER_GROUP
 from telethon import events
 from telethon.events import StopPropagation
 from telethon.tl.functions.contacts import BlockRequest
 from telethon.tl.functions.channels import LeaveChannelRequest, ExportInviteRequest, CreateChannelRequest, DeleteMessagesRequest
+from lmgtfy import lmgtfy
 
 @bot.on(events.NewMessage(outgoing=True, pattern='^\.timer '))
 @bot.on(events.MessageEdited(outgoing=True, pattern='^\.timer '))
@@ -115,3 +116,21 @@ async def cowsay(e):
         p+=subprocess.run(l, stdout=subprocess.PIPE).stdout.decode()
         p+='```'
         await e.edit(p)  
+
+@bot.on(events.NewMessage(pattern="^.lmg", outgoing=True))
+@bot.on(events.MessageEdited(pattern="^.lmg", outgoing=True))
+async def let_me_google_that_for_you(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        textx = await e.get_reply_message()
+        message = e.text
+        if message[8:]:
+            message = str(message[8:])
+        elif textx:
+            message = str(textx.message)
+        reply_text = 'http://lmgtfy.com/?s=g&iie=1&q=' + message.replace(" ", "+")
+        await e.edit(reply_text)
+        if LOGGER:
+            await bot.send_message(
+                LOGGER_GROUP,
+                "LMGTFY query " + message + " was executed successfully",
+            )     
