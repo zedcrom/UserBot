7f2ab4d9da4852ac887563464c3938028967773c
Yash Agarwal <42301695+Yash089610@users.noreply.github.com>
2019-02-09 19:05:16 +0700

    userbot: modules: memes: optimize TypeWriter
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -404,13 +404,12 @@ async def bluetext(e):
             "`Are you a stupid animal which is attracted to colours?`"
         )
 
-
 @bot.on(events.NewMessage(pattern='(?i).type'))
 @bot.on(events.MessageEdited(pattern='(?i).type'))
-async def typewriter(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        textx = await e.get_reply_message()
-        message = e.text
+async def typewriter(typew):
+    if not typew.text[0].isalpha() and typew.text[0] not in ("/", "#", "@", "!"):
+        textx = await typew.get_reply_message()
+        message = typew.text
 
         if message[6:]:
             message = str(message[6:])
@@ -419,18 +418,16 @@ async def typewriter(e):
             message = str(message.message)
         sleep_time = 0.03
         typing_symbol = "|"
-        index = 1
         old_text = ''
-        await e.edit(typing_symbol)
-        await asyncio.sleep(sleep_time)    
+        await typew.edit(typing_symbol)
+        await asyncio.sleep(sleep_time)
         for character in message:
             old_text = old_text + "" + character
             typing_text = old_text + "" + typing_symbol
-            await e.edit(typing_text)
+            await typew.edit(typing_text)
             await asyncio.sleep(sleep_time)
-            await e.edit(old_text)
+            await typew.edit(old_text)
             await asyncio.sleep(sleep_time)
-
 
 @bot.on(events.NewMessage(outgoing=True, pattern="^.fp$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.fp$"))
