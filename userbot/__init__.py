4d521d90c9a28ed31435ec5b7afebee9c1afe0c4
zedcrom <54992942+zedcrom@users.noreply.github.com>
2019-10-08 12:50:05 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -48,9 +48,9 @@ if CONFIG_CHECK:
     LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
     quit(1)
 
-API_KEY = os.environ.get("API_KEY", "1044101")
+API_KEY = os.environ.get("API_KEY", ")
 
-API_HASH = os.environ.get("API_HASH", "a629661df4f011357cd6f831b8434038" )
+API_HASH = os.environ.get("API_HASH", "" )
 
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
